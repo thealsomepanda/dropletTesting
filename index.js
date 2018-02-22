521bbfc28ebc7c92cee85c59784d72748904d745
thealsomepanda <thealsomepanda@gmail.com>
2018-02-22 12:54:30 -0500

    Fixed dirname in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res){
-    res.sendFile('index.html', {root: __dropletTesting + "/index.html"
+    res.sendFile('index.html', {root: __dirname + "/index.html"
     });
 });
 

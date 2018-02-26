a72daf622a9c6bc11311a59da21b00b9160c4aa2
thealsomepanda <thealsomepanda@gmail.com>
2018-02-26 11:54:20 -0500

    Added the formPage.html, making sure I am routing it right. If it works correctly you should be able to type in the ip and port plus formPage.html or just formPage       This is in perpetration to be able to take in info with forms and be able to handle it in a script.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var app = express();
 
+// dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const validator = require('express-validator');
+
+const app = express();
+
+// This handles request and respond functions, currently sends user to index.html
 app.get('/', function (req, res){
     res.sendFile('index.html', {root: __dirname + "/webPages"
+    });
+});
+
+app.get('/formPage', function (req, res){
+    res.sendFile('formPage.html', {root: __dirname +"/webpages"
     });
 });
 

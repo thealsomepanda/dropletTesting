2b98c524a014a10338c4eb8acb1fdbcde6e456b8
thealsomepanda <thealsomepanda@gmail.com>
2018-02-26 12:35:38 -0500

    Added code to see if I can get index.js to get info from the enter name form on form page.

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ const validator = require('express-validator');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // This handles request and respond functions, currently sends user to index.html
 app.get('/', function (req, res){
     res.sendFile('index.html', {root: __dirname + "/webPages"
     });
 });
-
+// Sends user to formPage.html
 app.get('/formPage', function (req, res){
     res.sendFile('formPage.html', {root: __dirname +"/webPages"
     });
 });
 
+// tells the server what port to listen to and prints to the console
 app.listen(3000);
-
 console.log("server is running on port 3000...");
+
+// this handles the form on formPage.html
+app.post('/formPage', function (req, res) {
+    res.send('You sent the name "' + res.body.name + '".');
+});
